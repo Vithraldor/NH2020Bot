366ad1516db0cdf58d9995248dc89d17793761e7
Ashley Rivera <ashley.rivera0019@gmail.com>
2020-11-07 15:57:26 -0500

    added msg bot will send upon joining a guild

--- NH2020Bot/NH2020Bot.py
+++ NH2020Bot/NH2020Bot.py
@@ -21,6 +21,14 @@ async def on_ready():
     
     startMessage = "with time"
     await client.change_presence(activity = discord.Streaming(name = startMessage, url = "https://github.com/Vithraldor"))
+
+# Message that the bot will send when it joins a server, prompting the user to enter their default timezone using tz!settimezone
+@client.event()
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send("Hi, I'm the **Timezone Conversion Reminder Bot**.\nTo get started, please set your timezone using **tz!settimezone**.")
+        break
     
 # Test command to see if the ping by ID function works (it does!)
 @client.command()
